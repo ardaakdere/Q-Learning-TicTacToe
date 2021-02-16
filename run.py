2b62480f6ac3629d550c3d5d6dbf12ae6b44edf4
Arda Akdere <54773283+ardaakdere@users.noreply.github.com>
2021-02-17 02:31:44 +0300

    Update run.py

--- run.py
+++ run.py
@@ -8,4 +8,6 @@ game = TicTacToe()
 # for player O --> suggestion = discount_factor = 0.5
 agent = Agent(game, 'X',discount_factor = 0.6, episode = 100000)
 
+agent.train_brain_x_byrandom()
+
 agent.play_with_human()
